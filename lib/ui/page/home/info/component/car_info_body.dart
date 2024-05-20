69b5d6fe27ee0b2eccb7366d3bf10edf8fdb9105
Jack Norris <hamuelxd@gmail.com>
2024-05-20 17:08:20 +0800

    Added missing registration ID

--- lib/ui/page/home/info/component/car_info_body.dart
+++ lib/ui/page/home/info/component/car_info_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:thyme_to_park_admin/service/registry/model/car.dart';
 import 'package:thyme_to_park_admin/ui/component/button.dart';
+import 'package:thyme_to_park_admin/ui/page/home/component/license_plate.dart';
 import 'package:thyme_to_park_admin/ui/page/home/info/component/car_info.dart';
 import 'package:thyme_to_park_admin/ui/util/corner.dart';
 
@@ -20,12 +21,17 @@ class CarInfoBody extends StatelessWidget {
   Widget build(final BuildContext context) {
     return Column(
       children: [
+        LicensePlate(registrationId: car.registrationId),
+        const SizedBox(height: 16),
         CarInfo(icon: Icons.sell, label: 'Make', value: car.make),
         const SizedBox(height: 16),
         CarInfo(icon: Icons.notes, label: 'Model', value: car.model),
         const SizedBox(height: 16),
         CarInfo(
-            icon: Icons.onetwothree, label: 'Year', value: car.year.toString()),
+          icon: Icons.onetwothree,
+          label: 'Year',
+          value: car.year.toString(),
+        ),
         const SizedBox(height: 16),
         CarInfo(icon: Icons.color_lens, label: 'Color', value: car.color),
         const SizedBox(height: 16),
